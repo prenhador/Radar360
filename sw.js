294541fa6ef73b81362e5eb32e78df26c0ede7b2
prenhador <51868303+prenhador@users.noreply.github.com>
2026-05-08 23:13:11 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+
+const CACHE_NAME = 'radar-cache-v1';
+const assets = ['./', 'manifest.json'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
